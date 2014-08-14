59877426739f676af75dd57342d94cd9c12e24b1
Stefan Hüsges <Tronsha@gmx.de>
2014-08-14 09:45:39 +0200

    Update slider.php

--- slider.php
+++ slider.php
@@ -10,54 +10,84 @@ Copyright: Stefan Hüsges
 License: MIT
 */
 
+defined('ABSPATH') or die("No script kiddies please!");
+
 function initSlider()
 {
-    wp_enqueue_script('slider.javascript', WP_PLUGIN_URL . '/wpslider/slider/js/slider.js', array('jquery'));
-    wp_enqueue_script('responsive.javascript', WP_PLUGIN_URL . '/wpslider/slider/js/responsive.js', array('jquery'));
-    wp_enqueue_style('slider.styles', WP_PLUGIN_URL . '/wpslider/slider/css/slider.css');
+    wp_enqueue_style('slider', WP_PLUGIN_URL . '/wp-slider/slider/css/slider.css');
+    wp_enqueue_script('slider', WP_PLUGIN_URL . '/wp-slider/slider/js/slider.js', array('jquery'));
+    wp_enqueue_script('slider-responsive', WP_PLUGIN_URL . '/wp-slider/slider/js/slider-responsive.js', array('jquery'));
 }
 
-function getSlider()
+function getSlider($att = array(), $content = null)
 {
-    $files = array();
-    if ($handle = opendir(realpath(dirname(__FILE__) . '/slider/images/'))) {
-        while (false !== ($file = readdir($handle))) {
-            if ($file != "." && $file != "..") {
-                $files[] = $file;
+    if (empty($content) === true) {
+        $content = '';
+        $files = array();
+        if ($handle = opendir(realpath(dirname(__FILE__) . '/slider/images/'))) {
+            while (false !== ($file = readdir($handle))) {
+                if ($file != "." && $file != "..") {
+                    $files[] = $file;
+                }
             }
+            closedir($handle);
         }
-        closedir($handle);
+
+        sort($files);
+
+        foreach ($files as $file) {
+            $content .= '<img src="' . WP_PLUGIN_URL . '/wp-slider/slider/images/' . $file . '" />';
+        }
     }
 
-    sort($files);
+    $options = '';
+
+    if (isset($att['delay']) === true) {
+        $options .= 'delay: ' . $att['delay'] . ', ';
+    }
+    if (isset($att['interval']) === true) {
+        $options .= 'interval: ' . $att['interval'] . ', ';
+    }
+
+    $style = '';
+    $change = '';
+    $position = '';
+
+    if (isset($att['height']) === true) {
+        $style .= 'height: ' . $att['height'] . 'px; ';
+        $change = 'top: ' . ($att['height'] / 2 - 8) . 'px; ';
+    }
+    if (isset($att['width']) === true) {
+        $style .= 'width: ' . $att['width'] . 'px; ';
+    }
+    if (isset($att['change']) === true && $att['change'] == 'false') {
+        $change .= 'display: none; ';
+    }
+    if (isset($att['position']) === true && $att['position'] == 'false') {
+        $position .= 'display: none; ';
+    }
 
     $output = '
-<div id="slider">
+<div id="slider" style="' . $style . '">
     <div class="slides">
-    ';
-
-    $counter = 0;
-    foreach ($files as $file) {
-        $output .= '<img class="image' . (++$counter) . '" src="' . WP_PLUGIN_URL . '/wpslider/slider/images/' . $file . '" />';
-    }
-
-    $output .= '
+    ' . $content . '
     </div>
-    <div class="prev"></div>
-    <div class="next"></div>
-    <div class="position"></div>
+    <div class="prev" style="' . $change . '"></div>
+    <div class="next" style="' . $change . '"></div>
+    <div class="position" style="' . $position . '"></div>
 </div>
 <script>
     jQuery(document).ready(function () {
-        jQuery("#slider").slider({delay: 1000, interval: 5000});
+        jQuery("#slider").slider({' . $options . '});
     });
 </script>
     ';
 
-    if ($counter > 0) {
+    if (empty($content) === false) {
         return $output;
     }
     return '';
 }
 
 add_action('init', 'initSlider');
+add_shortcode('slider', 'getSlider');
