eb72f14f1e17caea306affe95107ff434f5689ec
Stefan Hüsges <tronsha@gmail.com>
2014-08-31 21:02:14 +0200

    created a class for slider

--- slider.php
+++ slider.php
@@ -12,16 +12,29 @@ License: MIT
 
 defined('ABSPATH') or die("No script kiddies please!");
 
-function initSlider()
+class Slider
 {
-    wp_enqueue_style('slider', WP_PLUGIN_URL . '/wp-slider-plugin/slider/css/slider.css');
-    wp_enqueue_script('slider', WP_PLUGIN_URL . '/wp-slider-plugin/slider/js/slider.js', array('jquery'));
-    wp_enqueue_script('slider-responsive', WP_PLUGIN_URL . '/wp-slider-plugin/slider/js/responsive.js', array('jquery'));
-}
+    private $att = array();
+    private $content = null;
 
-function getSlider($att = array(), $content = null)
-{
-    if (empty($content) === true) {
+    public function __construct($att = array(), $content = null)
+    {
+        $this->att = $att;
+        $this->content = $content;
+    }
+
+    protected function cleanContent($content)
+    {
+        preg_match_all('/<img[^>]+>/i', $content, $matches);
+        $content = '';
+        foreach ($matches[0] as $image) {
+            $content .= $image;
+        }
+        return $content;
+    }
+
+    protected function getSlides()
+    {
         $content = '';
         $files = array();
         if ($handle = opendir(realpath(dirname(__FILE__) . '/slider/images/'))) {
@@ -32,78 +45,112 @@ function getSlider($att = array(), $content = null)
             }
             closedir($handle);
         }
-
         sort($files);
-
         foreach ($files as $file) {
             $content .= '<img src="' . WP_PLUGIN_URL . '/wp-slider-plugin/slider/images/' . $file . '" />';
         }
-    } else {
-        preg_match_all('/<img[^>]+>/i', do_shortcode($content), $matches);
-        $content = '';
-        foreach ($matches[0] as $image) {
-            $content .= $image;
+        return $content;
+    }
+
+    public function render()
+    {
+        $att = $this->att;
+
+        if (empty($this->content) === false) {
+            $content = do_shortcode($this->content);
+            $content = $this->cleanContent($content);
+        } else {
+            $content = $this->getSlides();
         }
-    }
 
-    $options = '';
+        $options = '';
 
-    if (isset($att['delay']) === true) {
-        $options .= 'delay: ' . $att['delay'] . ', ';
-    }
-    if (isset($att['interval']) === true) {
-        $options .= 'interval: ' . $att['interval'] . ', ';
-    }
-
-    $style = '';
-    $change = '';
-    $position = '';
-
-    if (isset($att['height']) === true) {
-        $style .= 'height: ' . $att['height'] . 'px; ';
-    }
-    if (isset($att['width']) === true) {
-        $style .= 'width: ' . $att['width'] . 'px; ';
-    }
-    if (isset($att['change']) === true && $att['change'] === 'false') {
-        $change .= 'display: none; ';
-    }
-    if (isset($att['position']) === true && $att['position'] === 'false') {
-        $position .= 'display: none; ';
-    }
-
-    $textBox = '';
-    if (isset($att['text']) === true) {
-        $textArray = explode('|', $att['text']);
-        $textBox = '<div class="text">';
-        foreach($textArray as $key => $sliderText) {
-            $textBox .= '<span class="' . ($key == 0 ? 'active' : '') . (empty($sliderText) ? ' hidden' : '') . '">' . $sliderText . '</span>';
+        if (isset($att['delay']) === true) {
+            $options .= 'delay: ' . $att['delay'] . ', ';
         }
-        $textBox .= '</div>';
-    }
+        if (isset($att['interval']) === true) {
+            $options .= 'interval: ' . $att['interval'] . ', ';
+        }
 
-    $output = '
-<div id="slider" class="slider" style="' . $style . '">
-    <div class="slides">
-    ' . $content . '
-    </div>
-    <div class="prev" style="' . $change . '"><div></div></div>
-    <div class="next" style="' . $change . '"><div></div></div>
-    <div class="position" style="' . $position . '"></div>
-    ' . $textBox . '
-</div>
-<script>
-    jQuery(document).ready(function () {
-        jQuery("#slider").slider({' . $options . '});
-    });
-</script>
-    ';
+        $style = '';
+        $change = '';
+        $position = '';
 
-    if (empty($content) === false) {
-        return $output;
+        if (isset($att['height']) === true) {
+            $style .= 'height: ' . $att['height'] . 'px; ';
+        }
+        if (isset($att['width']) === true) {
+            $style .= 'width: ' . $att['width'] . 'px; ';
+        }
+        if (isset($att['change']) === true && $att['change'] === 'false') {
+            $change .= 'display: none; ';
+        }
+        if (isset($att['position']) === true && $att['position'] === 'false') {
+            $position .= 'display: none; ';
+        }
+
+        $textBox = '';
+        if (isset($att['text']) === true) {
+            $textArray = explode('|', $att['text']);
+            $textBox = '<div class="text">';
+            foreach ($textArray as $key => $sliderText) {
+                $textBox .= '<span class="' . ($key == 0 ? 'active' : '') . (empty($sliderText) ? ' hidden' : '') . '">' . $sliderText . '</span>';
+            }
+            $textBox .= '</div>';
+        }
+
+        $output = '
+            <div id="slider" class="slider" style="' . $style . '">
+                <div class="slides">
+                ' . $content . '
+                </div>
+                <div class="prev" style="' . $change . '"><div></div></div>
+                <div class="next" style="' . $change . '"><div></div></div>
+                <div class="position" style="' . $position . '"></div>
+                ' . $textBox . '
+            </div>
+            <script>
+                jQuery(document).ready(function () {
+                    jQuery("#slider").slider({' . $options . '});
+                });
+            </script>
+        ';
+
+        if (empty($content) === false) {
+            return $output;
+        }
+        return '';
     }
-    return '';
 }
 
-add_action('init', 'initSlider');
-add_shortcode('slider', 'getSlider');
+add_action(
+    'init',
+    function () {
+        wp_enqueue_style(
+            'slider',
+            WP_PLUGIN_URL . '/wp-slider-plugin/slider/css/slider.css',
+            array(),
+            '1.0.0'
+        );
+        wp_enqueue_script(
+            'slider',
+            WP_PLUGIN_URL . '/wp-slider-plugin/slider/js/slider.js',
+            array('jquery'),
+            '1.0.0'
+        );
+        wp_enqueue_script(
+            'slider-responsive',
+            WP_PLUGIN_URL . '/wp-slider-plugin/slider/js/responsive.js',
+            array('jquery', 'slider'),
+            '1.0.0'
+        );
+    }
+);
+
+add_shortcode(
+    'slider',
+    function ($att = array(), $content = null) {
+        $slider = new Slider($att, $content);
+        return $slider->render();
+    }
+);
