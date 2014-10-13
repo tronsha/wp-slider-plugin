006f62575714b55e7cd571c8dc38576c2df1648c
Stefan Hüsges <Tronsha@gmx.de>
2014-10-13 15:07:37 +0200

    Update responsive.js

--- slider/js/responsive.js
+++ slider/js/responsive.js
@@ -11,10 +11,6 @@ var sliderResize = function() {
     $slider.each(function() {
         var $this = jQuery(this);
         $this.css('max-width', '100%');
-        var $previmg = $this.find('.prev > img');
-        var $nextimg = $this.find('.next > img');
-        $previmg.css('top', $previmg.height() / -2);
-        $nextimg.css('top', $nextimg.height() / -2);
         var $activeImage = $this.find('.slides .active');
         if ($activeImage.length > 0) {
             var imageWidth = $activeImage[0].naturalWidth;
