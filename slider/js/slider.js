b17e6777bf466cd3577db46edb26804bb016a6cc
Stefan Hüsges <Tronsha@gmx.de>
2014-10-16 15:39:00 +0200

    Update slider.js

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -4,18 +4,19 @@
     "use strict";
 
     var pluginName = "slider";
-    var that;
-    var timer;
     var defaults = {
         delay: 1000,
-        interval: 10000,
-        slide: 1,
-        slides: 0
+        interval: 10000
     };
 
     function Plugin(element, options) {
         this.element = element;
         this.settings = $.extend({}, defaults, options);
+        this.var = {
+            timer: undefined,
+            slide: 1,
+            slides: 0
+        };
         this._defaults = defaults;
         this._name = pluginName;
         this.init();
@@ -23,12 +24,12 @@
 
     Plugin.prototype = {
         init: function () {
-            that = this;
+            var self = this;
             var $slider = $(this.element);
             var $position = $slider.find('.position');
             $slider.children('.slides').find('img').each(function (index, element) {
-                that.settings.slides++;
-                if (that.settings.slides == that.settings.slide) {
+                self.var.slides++;
+                if (self.var.slides == self.var.slide) {
                     $(element).addClass('active').css('opacity', '1');
                     $position.append('<div class="points active"></div>');
                 } else {
@@ -38,54 +39,55 @@
             });
             $position.find('.points').each(function (index) {
                 $(this).click(function () {
-                    that.show(index + 1);
+                    self.show(index + 1);
                 });
             });
             $slider.hover(function () {
-                clearInterval(timer);
+                clearInterval(self.var.timer);
             }, function () {
-                that.auto();
+                self.auto();
             });
             $slider.find('.prev > *').click(function () {
-                that.prev();
+                self.prev();
             });
             $slider.find('.next > *').click(function () {
-                that.next();
+                self.next();
             });
-            that.auto();
+            this.auto();
         },
         auto: function () {
-            timer = setInterval(function () {
-                that.next();
-            }, that.settings.interval);
+            var self = this;
+            this.var.timer = setInterval(function () {
+                self.next();
+            }, this.settings.interval);
         },
         next: function () {
-            if (that.settings.slide < that.settings.slides) {
-                that.show(that.settings.slide + 1);
+            if (this.var.slide < this.var.slides) {
+                this.show(this.var.slide + 1);
             } else {
-                that.show(1);
+                this.show(1);
             }
         },
         prev: function () {
-            if (that.settings.slide > 1) {
-                that.show(that.settings.slide - 1);
+            if (this.var.slide > 1) {
+                this.show(this.var.slide - 1);
             } else {
-                that.show(that.settings.slides);
+                this.show(this.var.slides);
             }
         },
         show: function (slide) {
-            $(that.element).find('.slides img:nth-child(' + that.settings.slide + ')').stop().removeClass('active').animate({opacity: 0}, that.settings.delay);
-            $(that.element).find('.position .points:nth-child(' + that.settings.slide + ')').removeClass('active');
-            $(that.element).find('.text span:nth-child(' + that.settings.slide + ')').removeClass('active');
-            that.settings.slide = slide;
-            $(that.element).find('.slides img:nth-child(' + that.settings.slide + ')').stop().addClass('active').animate({opacity: 1}, that.settings.delay);
-            $(that.element).find('.position .points:nth-child(' + that.settings.slide + ')').addClass('active');
-            $(that.element).find('.text span:nth-child(' + that.settings.slide + ')').addClass('active');
+            $(this.element).find('.slides img:nth-child(' + this.var.slide + ')').stop().removeClass('active').animate({opacity: 0}, this.settings.delay);
+            $(this.element).find('.position .points:nth-child(' + this.var.slide + ')').removeClass('active');
+            $(this.element).find('.text span:nth-child(' + this.var.slide + ')').removeClass('active');
+            this.var.slide = slide;
+            $(this.element).find('.slides img:nth-child(' + this.var.slide + ')').stop().addClass('active').animate({opacity: 1}, this.settings.delay);
+            $(this.element).find('.position .points:nth-child(' + this.var.slide + ')').addClass('active');
+            $(this.element).find('.text span:nth-child(' + this.var.slide + ')').addClass('active');
             return this;
         }
     };
 
-    $.fn[ pluginName ] = function (options) {
+    $.fn[pluginName] = function (options) {
         this.each(function () {
             if (!$.data(this, "plugin_" + pluginName)) {
                 $.data(this, "plugin_" + pluginName, new Plugin(this, options));
