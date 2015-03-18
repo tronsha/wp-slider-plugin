d71a9748ed308d6629d85a94ab09ccd2b2137c06
Stefan Hüsges <tronsha@gmx.de>
2015-03-18 12:39:46 +0100

    WordPress Coding Standards

--- slider.php
+++ slider.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: UPA Slider
  * Plugin URI:  https://github.com/tronsha/wp-slider-plugin/blob/upa/README.md
@@ -10,181 +11,179 @@
  * License:     MIT
  * License URI: https://raw.githubusercontent.com/tronsha/wp-slider-plugin/master/LICENSE
  */
+class Slider {
+	private static $id = 0;
+	private static $posts = null;
+	private $att = array();
+	private $content = null;
 
-class Slider
-{
-    private static $id = 0;
-    private static $posts = null;
-    private $att = array();
-    private $content = null;
+	public function __construct( $att = array(), $content = null ) {
+		$this->att     = $att;
+		$this->content = $content;
+		self::$id ++;
+	}
 
-    public function __construct($att = array(), $content = null)
-    {
-        $this->att = $att;
-        $this->content = $content;
-        self::$id++;
-    }
+	protected function cleanContent( $content ) {
+		preg_match_all( '/<img[^>]+>/i', $content, $matches );
+		$content = '';
+		foreach ( $matches[0] as $image ) {
+			$content .= $image;
+		}
 
-    protected function cleanContent($content)
-    {
-        preg_match_all('/<img[^>]+>/i', $content, $matches);
-        $content = '';
-        foreach ($matches[0] as $image) {
-            $content .= $image;
-        }
-        return $content;
-    }
+		return $content;
+	}
 
-    protected function getPosts($x = true)
-    {
-        if (self::$posts === null && $x === true) {
-            self::$posts = get_posts(array(
-                'offset' => 0,
-                'category_name' => 'slides',
-                'posts_per_page' => -1,
-                'orderby' => 'ID',
-                'order' => 'ASC',
-            ));
-        }
-        return self::$posts;
-    }
+	protected function getPosts( $x = true ) {
+		if ( self::$posts === null && $x === true ) {
+			self::$posts = get_posts( array(
+				'offset'         => 0,
+				'category_name'  => 'slides',
+				'posts_per_page' => - 1,
+				'orderby'        => 'ID',
+				'order'          => 'ASC',
+			) );
+		}
 
-    protected function getSlidesFromPosts()
-    {
-        $slides = '';
-        $posts = $this->getPosts();
-        foreach ($posts as $post) {
-            $image_src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
-            $slides .= '<img src="' . $image_src[0] . '" alt="' . $post->post_title . '">';
-        }
-        return $slides;
-    }
+		return self::$posts;
+	}
 
-    protected function getSlidesFromDir()
-    {
-        $slides = '';
-        $path = 'slider/slides/';
-        $dir = realpath(dirname(__FILE__) . '/' . $path) . '/';
-        $type = array('png', 'jpg');
-        $files = glob($dir . '*.{' . implode(',', $type) . '}', GLOB_BRACE);
-        if ($files !== false) {
-            foreach ($files as $file) {
-                $slides .= '<img src="' . plugin_dir_url(__FILE__) . $path . basename($file) . '" alt="Slider Image">';
-            }
-        }
-        return $slides;
-    }
+	protected function getSlidesFromPosts() {
+		$slides = '';
+		$posts  = $this->getPosts();
+		foreach ( $posts as $post ) {
+			$image_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+			$slides .= '<img src="' . $image_src[0] . '" alt="' . $post->post_title . '">';
+		}
 
-    protected function getSlides()
-    {
-        if (empty($this->content) === false) {
-            $content = do_shortcode($this->content);
-            $slides = $this->cleanContent($content);
-        } else {
-            $slides = $this->getSlidesFromPosts();
-            if (empty($slides) === true) {
-                $slides = $this->getSlidesFromDir();
-            }
-        }
-        return $slides;
-    }
+		return $slides;
+	}
 
-    protected function getSliderOptions()
-    {
-        $options = '';
-        if (isset($this->att['delay']) === true) {
-            $options .= 'delay: ' . $this->att['delay'] . ', ';
-        }
-        if (isset($this->att['interval']) === true) {
-            $options .= 'interval: ' . $this->att['interval'] . ', ';
-        }
-        return $options;
-    }
+	protected function getSlidesFromDir() {
+		$slides = '';
+		$path   = 'slider/slides/';
+		$dir    = realpath( dirname( __FILE__ ) . '/' . $path ) . '/';
+		$type   = array( 'png', 'jpg' );
+		$files  = glob( $dir . '*.{' . implode( ',', $type ) . '}', GLOB_BRACE );
+		if ( $files !== false ) {
+			foreach ( $files as $file ) {
+				$slides .= '<img src="' . plugin_dir_url( __FILE__ ) . $path . basename( $file ) . '" alt="Slider Image">';
+			}
+		}
 
-    protected function getSliderStyle()
-    {
-        $style = '';
-        if (isset($this->att['height']) === true) {
-            $style .= 'height: ' . $this->att['height'] . 'px; ';
-        }
-        if (isset($this->att['width']) === true) {
-            $style .= 'width: ' . $this->att['width'] . 'px; ';
-        }
-        return $style;
-    }
+		return $slides;
+	}
 
-    protected function getButtonPrev()
-    {
-        if (isset($this->att['change']) === true && $this->att['change'] === 'false') {
-            return '';
-        }
-        $prevButton = '<div class="prev">';
-        if (file_exists(get_template_directory() . '/images/slider/prev.png')) {
-            $prevButton .= '<img src="' . get_template_directory_uri() . '/images/slider/prev.png" alt="prev">';
-        } elseif (file_exists(plugin_dir_path(__FILE__) . 'slider/images/prev.png')) {
-            $prevButton .= '<img src="' . plugin_dir_url(__FILE__) . 'slider/images/prev.png" alt="prev">';
-        } else {
-            $prevButton .= '<div>&#160;</div>';
-        }
-        $prevButton .= '</div>';
-        return $prevButton;
-    }
+	protected function getSlides() {
+		if ( empty( $this->content ) === false ) {
+			$content = do_shortcode( $this->content );
+			$slides  = $this->cleanContent( $content );
+		} else {
+			$slides = $this->getSlidesFromPosts();
+			if ( empty( $slides ) === true ) {
+				$slides = $this->getSlidesFromDir();
+			}
+		}
 
-    protected function getButtonNext()
-    {
-        if (isset($this->att['change']) === true && $this->att['change'] === 'false') {
-            return '';
-        }
-        $nextButton = '<div class="next">';
-        if (file_exists(get_template_directory() . '/images/slider/next.png')) {
-            $nextButton .= '<img src="' . get_template_directory_uri() . '/images/slider/next.png" alt="next">';
-        } elseif (file_exists(plugin_dir_path(__FILE__) . 'slider/images/next.png')) {
-            $nextButton .= '<img src="' . plugin_dir_url(__FILE__) . 'slider/images/next.png" alt="next">';
-        } else {
-            $nextButton .= '<div>&#160;</div>';
-        }
-        $nextButton .= '</div>';
-        return $nextButton;
-    }
+		return $slides;
+	}
 
-    protected function getPositionBar()
-    {
-        if (isset($this->att['position']) === true && $this->att['position'] === 'false') {
-            return '';
-        }
-        return '<div class="position"></div>';
-    }
+	protected function getSliderOptions() {
+		$options = '';
+		if ( isset( $this->att['delay'] ) === true ) {
+			$options .= 'delay: ' . $this->att['delay'] . ', ';
+		}
+		if ( isset( $this->att['interval'] ) === true ) {
+			$options .= 'interval: ' . $this->att['interval'] . ', ';
+		}
 
-    protected function getText()
-    {
-        if (isset($this->att['text']) === true) {
-            $textArray = explode('|', $this->att['text']);
-            $textBox = '<div class="text">';
-            foreach ($textArray as $key => $sliderText) {
-                $textBox .= '<span class="' . ($key == 0 ? 'active' : '') . (empty($sliderText) ? ' hidden' : '') . '">' . $sliderText . '</span>';
-            }
-            $textBox .= '</div>';
-            return $textBox;
-        } else {
-            $posts = $this->getPosts(false);
-            if (empty($posts) === false) {
-                $textBox = '<div class="text">';
-                $first = true;
-                foreach ($posts as $post) {
-                    $textBox .= '<span class="' . ($first === true ? 'active' : '') . (empty($post->post_content) ? ' hidden' : '') . '">' . $post->post_content . '</span>';
-                    $first = false;
-                }
-                $textBox .= '</div>';
-                return $textBox;
-            }
-        }
-        return '';
-    }
+		return $options;
+	}
 
-    public function render()
-    {
-        $slides = $this->getSlides();
-        $output = '
+	protected function getSliderStyle() {
+		$style = '';
+		if ( isset( $this->att['height'] ) === true ) {
+			$style .= 'height: ' . $this->att['height'] . 'px; ';
+		}
+		if ( isset( $this->att['width'] ) === true ) {
+			$style .= 'width: ' . $this->att['width'] . 'px; ';
+		}
+
+		return $style;
+	}
+
+	protected function getButtonPrev() {
+		if ( isset( $this->att['change'] ) === true && $this->att['change'] === 'false' ) {
+			return '';
+		}
+		$prevButton = '<div class="prev">';
+		if ( file_exists( get_template_directory() . '/images/slider/prev.png' ) ) {
+			$prevButton .= '<img src="' . get_template_directory_uri() . '/images/slider/prev.png" alt="prev">';
+		} elseif ( file_exists( plugin_dir_path( __FILE__ ) . 'slider/images/prev.png' ) ) {
+			$prevButton .= '<img src="' . plugin_dir_url( __FILE__ ) . 'slider/images/prev.png" alt="prev">';
+		} else {
+			$prevButton .= '<div>&#160;</div>';
+		}
+		$prevButton .= '</div>';
+
+		return $prevButton;
+	}
+
+	protected function getButtonNext() {
+		if ( isset( $this->att['change'] ) === true && $this->att['change'] === 'false' ) {
+			return '';
+		}
+		$nextButton = '<div class="next">';
+		if ( file_exists( get_template_directory() . '/images/slider/next.png' ) ) {
+			$nextButton .= '<img src="' . get_template_directory_uri() . '/images/slider/next.png" alt="next">';
+		} elseif ( file_exists( plugin_dir_path( __FILE__ ) . 'slider/images/next.png' ) ) {
+			$nextButton .= '<img src="' . plugin_dir_url( __FILE__ ) . 'slider/images/next.png" alt="next">';
+		} else {
+			$nextButton .= '<div>&#160;</div>';
+		}
+		$nextButton .= '</div>';
+
+		return $nextButton;
+	}
+
+	protected function getPositionBar() {
+		if ( isset( $this->att['position'] ) === true && $this->att['position'] === 'false' ) {
+			return '';
+		}
+
+		return '<div class="position"></div>';
+	}
+
+	protected function getText() {
+		if ( isset( $this->att['text'] ) === true ) {
+			$textArray = explode( '|', $this->att['text'] );
+			$textBox   = '<div class="text">';
+			foreach ( $textArray as $key => $sliderText ) {
+				$textBox .= '<span class="' . ( $key == 0 ? 'active' : '' ) . ( empty( $sliderText ) ? ' hidden' : '' ) . '">' . $sliderText . '</span>';
+			}
+			$textBox .= '</div>';
+
+			return $textBox;
+		} else {
+			$posts = $this->getPosts( false );
+			if ( empty( $posts ) === false ) {
+				$textBox = '<div class="text">';
+				$first   = true;
+				foreach ( $posts as $post ) {
+					$textBox .= '<span class="' . ( $first === true ? 'active' : '' ) . ( empty( $post->post_content ) ? ' hidden' : '' ) . '">' . $post->post_content . '</span>';
+					$first = false;
+				}
+				$textBox .= '</div>';
+
+				return $textBox;
+			}
+		}
+
+		return '';
+	}
+
+	public function render() {
+		$slides = $this->getSlides();
+		$output = '
             <div id="slider-' . self::$id . '" class="slider" style="' . $this->getSliderStyle() . '">
                 <div class="slides">
                 ' . $slides . '
@@ -200,46 +199,46 @@ class Slider
                 });
             </script>
         ';
-        if (empty($slides) === false) {
-            return $output;
-        }
-        return '';
-    }
+		if ( empty( $slides ) === false ) {
+			return $output;
+		}
+
+		return '';
+	}
 }
 
-function initSlider()
-{
-    if (!is_admin()) {
-        wp_register_style(
-            'slider',
-            plugin_dir_url(__FILE__) . 'slider/css/slider.css',
-            array(),
-            '1.0.0'
-        );
-        wp_register_script(
-            'slider',
-            plugin_dir_url(__FILE__) . 'slider/js/slider.js',
-            array('jquery'),
-            '1.0.0'
-        );
-        wp_register_script(
-            'slider-responsive',
-            plugin_dir_url(__FILE__) . 'slider/js/responsive.js',
-            array('jquery', 'slider'),
-            '1.0.0'
-        );
-        wp_enqueue_style('slider');
-        wp_enqueue_script('slider');
-        wp_enqueue_script('slider-responsive');
-    }
-    add_theme_support('post-thumbnails');
+function initSlider() {
+	if ( ! is_admin() ) {
+		wp_register_style(
+			'slider',
+			plugin_dir_url( __FILE__ ) . 'slider/css/slider.css',
+			array(),
+			'1.0.0'
+		);
+		wp_register_script(
+			'slider',
+			plugin_dir_url( __FILE__ ) . 'slider/js/slider.js',
+			array( 'jquery' ),
+			'1.0.0'
+		);
+		wp_register_script(
+			'slider-responsive',
+			plugin_dir_url( __FILE__ ) . 'slider/js/responsive.js',
+			array( 'jquery', 'slider' ),
+			'1.0.0'
+		);
+		wp_enqueue_style( 'slider' );
+		wp_enqueue_script( 'slider' );
+		wp_enqueue_script( 'slider-responsive' );
+	}
+	add_theme_support( 'post-thumbnails' );
 }
 
-function shortcodeSlider($att = array(), $content = null)
-{
-    $slider = new Slider($att, $content);
-    return $slider->render();
+function shortcodeSlider( $att = array(), $content = null ) {
+	$slider = new Slider( $att, $content );
+
+	return $slider->render();
 }
 
-add_action('init', 'initSlider');
-add_shortcode('slider', 'shortcodeSlider');
+add_action( 'init', 'initSlider' );
+add_shortcode( 'slider', 'shortcodeSlider' );
--- slider/js/slider.js
+++ slider/js/slider.js
@@ -1,99 +1,103 @@
 ;
-(function ($, window, document, undefined) {
+(
+	function ( $, window, document, undefined ) {
 
-    "use strict";
+		"use strict";
 
-    var pluginName = "slider";
-    var defaults = {
-        delay: 1000,
-        interval: 10000
-    };
+		var pluginName = "slider";
+		var defaults = {
+			delay: 1000,
+			interval: 10000
+		};
 
-    function Plugin(element, options) {
-        this.element = element;
-        this.settings = $.extend({}, defaults, options);
-        this.vari = {
-            timer: undefined,
-            slide: 1,
-            slides: 0
-        };
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.init();
-    }
+		function Plugin( element, options ) {
+			this.element = element;
+			this.settings = $.extend( {}, defaults, options );
+			this.vari = {
+				timer: undefined,
+				slide: 1,
+				slides: 0
+			};
+			this._defaults = defaults;
+			this._name = pluginName;
+			this.init();
+		}
 
-    Plugin.prototype = {
-        init: function () {
-            var self = this;
-            var $slider = $(this.element);
-            var $position = $slider.find('.position');
-            $slider.children('.slides').find('img').each(function (index, element) {
-                self.vari.slides++;
-                if (self.vari.slides == self.vari.slide) {
-                    $(element).addClass('active').css('opacity', '1');
-                    $position.append('<div class="points active"></div>');
-                } else {
-                    $(element).css('opacity', '0');
-                    $position.append('<div class="points"></div>');
-                }
-            });
-            $position.find('.points').each(function (index) {
-                $(this).click(function () {
-                    self.show(index + 1);
-                });
-            });
-            $slider.hover(function () {
-                clearInterval(self.vari.timer);
-            }, function () {
-                self.auto();
-            });
-            $slider.find('.prev > *').click(function () {
-                self.prev();
-            });
-            $slider.find('.next > *').click(function () {
-                self.next();
-            });
-            this.auto();
-        },
-        auto: function () {
-            var self = this;
-            this.vari.timer = setInterval(function () {
-                self.next();
-            }, this.settings.interval);
-        },
-        next: function () {
-            if (this.vari.slide < this.vari.slides) {
-                this.show(this.vari.slide + 1);
-            } else {
-                this.show(1);
-            }
-        },
-        prev: function () {
-            if (this.vari.slide > 1) {
-                this.show(this.vari.slide - 1);
-            } else {
-                this.show(this.vari.slides);
-            }
-        },
-        show: function (slide) {
-            $(this.element).find('.slides img:nth-child(' + this.vari.slide + ')').stop().removeClass('active').animate({opacity: 0}, this.settings.delay);
-            $(this.element).find('.position .points:nth-child(' + this.vari.slide + ')').removeClass('active');
-            $(this.element).find('.text span:nth-child(' + this.vari.slide + ')').removeClass('active');
-            this.vari.slide = slide;
-            $(this.element).find('.slides img:nth-child(' + this.vari.slide + ')').stop().addClass('active').animate({opacity: 1}, this.settings.delay);
-            $(this.element).find('.position .points:nth-child(' + this.vari.slide + ')').addClass('active');
-            $(this.element).find('.text span:nth-child(' + this.vari.slide + ')').addClass('active');
-            return this;
-        }
-    };
+		Plugin.prototype = {
+			init: function () {
+				var self = this;
+				var $slider = $( this.element );
+				var $position = $slider.find( '.position' );
+				$slider.children( '.slides' ).find( 'img' ).each( function ( index, element ) {
+					self.vari.slides ++;
+					if ( self.vari.slides == self.vari.slide ) {
+						$( element ).addClass( 'active' ).css( 'opacity', '1' );
+						$position.append( '<div class="points active"></div>' );
+					} else {
+						$( element ).css( 'opacity', '0' );
+						$position.append( '<div class="points"></div>' );
+					}
+				} );
+				$position.find( '.points' ).each( function ( index ) {
+					$( this ).click( function () {
+						self.show( index + 1 );
+					} );
+				} );
+				$slider.hover( function () {
+					clearInterval( self.vari.timer );
+				}, function () {
+					self.auto();
+				} );
+				$slider.find( '.prev > *' ).click( function () {
+					self.prev();
+				} );
+				$slider.find( '.next > *' ).click( function () {
+					self.next();
+				} );
+				this.auto();
+			},
+			auto: function () {
+				var self = this;
+				this.vari.timer = setInterval( function () {
+					self.next();
+				}, this.settings.interval );
+			},
+			next: function () {
+				if ( this.vari.slide < this.vari.slides ) {
+					this.show( this.vari.slide + 1 );
+				} else {
+					this.show( 1 );
+				}
+			},
+			prev: function () {
+				if ( this.vari.slide > 1 ) {
+					this.show( this.vari.slide - 1 );
+				} else {
+					this.show( this.vari.slides );
+				}
+			},
+			show: function ( slide ) {
+				$( this.element ).find( '.slides img:nth-child(' + this.vari.slide + ')' ).stop().removeClass( 'active' ).animate( {opacity: 0},
+					this.settings.delay );
+				$( this.element ).find( '.position .points:nth-child(' + this.vari.slide + ')' ).removeClass( 'active' );
+				$( this.element ).find( '.text span:nth-child(' + this.vari.slide + ')' ).removeClass( 'active' );
+				this.vari.slide = slide;
+				$( this.element ).find( '.slides img:nth-child(' + this.vari.slide + ')' ).stop().addClass( 'active' ).animate( {opacity: 1},
+					this.settings.delay );
+				$( this.element ).find( '.position .points:nth-child(' + this.vari.slide + ')' ).addClass( 'active' );
+				$( this.element ).find( '.text span:nth-child(' + this.vari.slide + ')' ).addClass( 'active' );
+				return this;
+			}
+		};
 
-    $.fn[pluginName] = function (options) {
-        this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
-            }
-        });
-        return this;
-    };
+		$.fn[pluginName] = function ( options ) {
+			this.each( function () {
+				if ( ! $.data( this, "plugin_" + pluginName ) ) {
+					$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+				}
+			} );
+			return this;
+		};
 
-})(jQuery, window, document);
+	}
+)( jQuery, window, document );
